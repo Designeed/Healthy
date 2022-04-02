ff6e16a69da430dd0890cd23c33b247a285f95bf
Designeed <v.burczev2013@yandex.ru>
2022-04-02 21:07:28 +0700

    build project structure MVVM and added 3 fragments

--- app/src/main/java/com/example/healthy/view/MainActivity.kt
+++ app/src/main/java/com/example/healthy/view/MainActivity.kt
@@ -1,7 +1,8 @@
-package com.example.healthy
+package com.example.healthy.view
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.example.healthy.R
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
--- app/src/main/java/com/example/healthy/view/SplashScreen.kt
+++ app/src/main/java/com/example/healthy/view/SplashScreen.kt
@@ -1,4 +1,4 @@
-package com.example.healthy
+package com.example.healthy.view
 
 import android.content.Intent
 import android.os.Bundle
