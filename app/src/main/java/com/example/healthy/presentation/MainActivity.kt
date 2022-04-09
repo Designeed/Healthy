44b5a5e75dbe7f711f9a8892b934702efffb1610
Designeed <v.burczev2013@yandex.ru>
2022-04-09 19:44:16 +0700

    added new implementation for bottom navigation

--- app/src/main/java/com/example/healthy/presentation/MainActivity.kt
+++ app/src/main/java/com/example/healthy/presentation/MainActivity.kt
@@ -2,37 +2,26 @@ package com.example.healthy.presentation
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
+import androidx.navigation.ui.AppBarConfiguration
+import androidx.navigation.ui.setupActionBarWithNavController
+import androidx.navigation.ui.setupWithNavController
 import com.example.healthy.R
-import com.example.healthy.presentation.food.FoodFragment
-import com.example.healthy.presentation.journal.JournalFragment
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
-    private lateinit var currentFragment: Fragment
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        setTitle(R.string.actionBar_journal)
-        supportFragmentManager.beginTransaction().replace(R.id.containerView, JournalFragment()).commit()
-        val bottomNav: BottomNavigationView = findViewById(R.id.bottom_navigation)
-        bottomNav.setOnItemSelectedListener(navListener)
-    }
 
-    private val navListener = BottomNavigationView.OnNavigationItemSelectedListener {
-        when(it.itemId){
-            R.id.ic_journal -> {
-                currentFragment = JournalFragment()
-                setTitle(R.string.actionBar_journal)
-            }
+        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+        val navController = findNavController(R.id.containerView)
 
-            R.id.ic_food -> {
-              currentFragment = FoodFragment()
-                setTitle(R.string.actionBar_food)
-            }
-        }
-        supportFragmentManager.beginTransaction().replace(R.id.containerView, currentFragment).commit()
-        true
+        bottomNavigationView.setupWithNavController(navController)
+
+        val appBarConfiguration = AppBarConfiguration(setOf(R.id.fragment_food ,R.id.fragment_journal))
+        setupActionBarWithNavController(navController, appBarConfiguration)
     }
 }
