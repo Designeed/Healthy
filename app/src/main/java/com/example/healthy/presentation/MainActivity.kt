eadaa8c505c9959f4706e023190149a657e2aa9d
Designeed <v.burczev2013@yandex.ru>
2022-04-10 00:13:38 +0700

    added bottom app bar

--- app/src/main/java/com/example/healthy/presentation/MainActivity.kt
+++ app/src/main/java/com/example/healthy/presentation/MainActivity.kt
@@ -7,21 +7,23 @@ import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
 import androidx.navigation.ui.setupWithNavController
 import com.example.healthy.R
+import com.example.healthy.R.id.bottomNavigationView
 import com.google.android.material.bottomnavigation.BottomNavigationView
 
 class MainActivity : AppCompatActivity() {
-
-
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val bottomNavigationView = findViewById<BottomNavigationView>(R.id.bottom_navigation)
+        findViewById<BottomNavigationView>(bottomNavigationView).menu.getItem(1).isEnabled = false
+
+        val bottomNavigationView: BottomNavigationView = findViewById(bottomNavigationView)
         val navController = findNavController(R.id.containerView)
 
         bottomNavigationView.setupWithNavController(navController)
 
-        val appBarConfiguration = AppBarConfiguration(setOf(R.id.fragment_food ,R.id.fragment_journal))
+        val appBarConfiguration = AppBarConfiguration(setOf(R.id.fragment_journal ,R.id.fragment_food))
+
         setupActionBarWithNavController(navController, appBarConfiguration)
     }
 }
