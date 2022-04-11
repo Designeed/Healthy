6a527d45f758551c44fa3cef690d1d8f9131d1a9
Designeed <v.burczev2013@yandex.ru>
2022-04-12 01:13:20 +0700

    added navigation in app

---
+++ app/src/main/java/com/example/healthy/presentation/fragments/journal/AddJournalFragment.kt
@@ -0,0 +1,21 @@
+package com.example.healthy.presentation.fragments.journal
+
+import android.os.Bundle
+import android.view.*
+import androidx.fragment.app.Fragment
+import com.example.healthy.R
+
+class AddJournalFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        setHasOptionsMenu(true)
+        return inflater.inflate(R.layout.fragment_add_journal, container, false)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        inflater.inflate(R.menu.action_bar_settings, menu)
+        super.onCreateOptionsMenu(menu, inflater)
+    }
+}
