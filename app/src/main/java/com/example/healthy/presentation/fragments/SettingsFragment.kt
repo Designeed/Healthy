ee548ee61e12f8545d60b9fc5278b25bcbfa58f2
Designeed <v.burczev2013@yandex.ru>
2022-04-12 01:14:16 +0700

    added navigation in app

--- app/src/main/java/com/example/healthy/presentation/fragments/SettingsFragment.kt
+++ app/src/main/java/com/example/healthy/presentation/fragments/SettingsFragment.kt
@@ -1,4 +1,4 @@
-package com.example.healthy.presentation.food
+package com.example.healthy.presentation.fragments
 
 import android.os.Bundle
 import androidx.fragment.app.Fragment
@@ -7,12 +7,11 @@ import android.view.View
 import android.view.ViewGroup
 import com.example.healthy.R
 
- class FoodFragment : Fragment() {
+class SettingsFragment : Fragment() {
     override fun onCreateView(
         inflater: LayoutInflater, container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_food, container, false)
+        return inflater.inflate(R.layout.fragment_settings, container, false)
     }
 }
